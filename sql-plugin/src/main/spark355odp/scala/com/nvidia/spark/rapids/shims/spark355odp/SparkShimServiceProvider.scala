96e9e2e1d2f3314918613c6e1c370b51073dda02
manishsinghmowall <manishsingh@acceldata.io>
2026-04-17 17:35:04 +0200

    [Automated] Changed version from '3.3.6.3-101' to '3.3.6.3-102'

--- sql-plugin/src/main/spark355odp/scala/com/nvidia/spark/rapids/shims/spark355odp/SparkShimServiceProvider.scala
+++ sql-plugin/src/main/spark355odp/scala/com/nvidia/spark/rapids/shims/spark355odp/SparkShimServiceProvider.scala
@@ -22,8 +22,8 @@ package com.nvidia.spark.rapids.shims.spark355odp
 import com.nvidia.spark.rapids.{AcceldataShimVersion, ShimVersion}
 
 object SparkShimServiceProvider {
-  val VERSION = AcceldataShimVersion(3, 5, 5, "3.3.6.3-101")
-  val ODP_BASE_VERSION = "3.5.5.3.3.6.3-101"
+  val VERSION = AcceldataShimVersion(3, 5, 5, "3.3.6.3-102")
+  val ODP_BASE_VERSION = "3.5.5.3.3.6.3-102"
 }
 
 class SparkShimServiceProvider extends com.nvidia.spark.rapids.SparkShimServiceProvider {
