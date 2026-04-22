b039a7a50f52894a2c0950a00295d3b5087bd9b1
manishsinghmowall <manishsingh@acceldata.io>
2026-04-22 13:49:42 +0200

    [Automated] Changed version from '3.2.3.5-2' to '3.2.3.5-201'

--- sql-plugin/src/test/spark355odp/scala/com/nvidia/spark/rapids/shims/spark355odp/SparkShimsSuite.scala
+++ sql-plugin/src/test/spark355odp/scala/com/nvidia/spark/rapids/shims/spark355odp/SparkShimsSuite.scala
@@ -24,7 +24,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class SparkShimsSuite extends AnyFunSuite with FQSuiteName {
   test("spark shims version") {
-    assert(ShimLoader.getShimVersion === AcceldataShimVersion(3, 5, 5, "3.2.3.5-2"))
+    assert(ShimLoader.getShimVersion === AcceldataShimVersion(3, 5, 5, "3.2.3.5-201"))
   }
 
   test("shuffle manager class") {
